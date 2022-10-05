3905ae58b67720fbe2c625a7a83c3a170498ecfd
TungPhan2808 <phanmanhtung123@gmail.com>
2022-10-05 09:23:22 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import React, { Component } from 'react';
-
-
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+//App is a React component class that receives parameters and calls props (properties)
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div >
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">Restorable Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
